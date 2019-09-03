9e2218c30aed7606882904302f10821c5172eb91
Zulifqar Ahmad <zulifqar.ahmad@hotmail.com>
2019-09-03 14:30:59 -0500

    formatted all files

--- protractor.conf.js
+++ protractor.conf.js
@@ -11,14 +11,12 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
-  specs: [
-    './e2e/**/*.e2e-spec.ts'
-  ],
+  specs: ['./e2e/**/*.e2e-spec.ts'],
   capabilities: {
-    'browserName': 'chrome',
-    'chromeOptions': {
-      'args': ['show-fps-counter=true', '--no-sandbox']
-    }
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['show-fps-counter=true', '--no-sandbox'],
+    },
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
@@ -26,20 +24,22 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function() {},
   },
-  onPrepare: async() => {
+  onPrepare: async () => {
     require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
+      project: 'e2e/tsconfig.e2e.json',
     });
 
     jasmine.getEnv().addReporter(new SpecReporter({ acspec: { displayStacktrace: true } }));
   },
-  plugins: [{
-    package: 'protractor-console-plugin',
-    failOnWarning: false,
-    failOnError: true,
-    logWarnings: true,
-    exclude: []
-  }]
+  plugins: [
+    {
+      package: 'protractor-console-plugin',
+      failOnWarning: false,
+      failOnError: true,
+      logWarnings: true,
+      exclude: [],
+    },
+  ],
 };
