0f654d6eb6d8d1c6c0c9f1339f247ac4f14dab88
Zulifqar Ahmad <zulifqar.ahmad@hotmail.com>
2019-09-04 19:34:51 -0500

    more work around career success page

--- src/app/pages/dashboard/status-card/status-card-placeholder.component.ts
+++ src/app/pages/dashboard/status-card/status-card-placeholder.component.ts
@@ -10,10 +10,9 @@ import { Component } from '@angular/core';
   selector: 'ngx-status-card-placeholder',
   styleUrls: ['./status-card.component.scss'],
   template: `
-    <nb-card [ngClass]="{'off': true}">
+    <nb-card [ngClass]="{ off: true }">
       <div class="icon-container">
-        <div class="icon primary">
-        </div>
+        <div class="icon primary"></div>
       </div>
       <div class="details">
         <div class="title"></div>
@@ -22,5 +21,4 @@ import { Component } from '@angular/core';
     </nb-card>
   `,
 })
-export class StatusCardPlaceholderComponent {
-}
+export class StatusCardPlaceholderComponent {}
--- src/app/pages/dashboard/status-card/status-card.component.ts
+++ src/app/pages/dashboard/status-card/status-card.component.ts
@@ -10,22 +10,20 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   selector: 'ngx-status-card',
   styleUrls: ['./status-card.component.scss'],
   template: `
-    <nb-card (click)="onClick()" [ngClass]="{'off': !on}">
-      <div class="icon-container">
+    <nb-card (click)="onClick()" [ngClass]="{ off: !on }">
+      <div class="net-work icon-container">
         <div class="icon status-{{ type }}">
           <ng-content></ng-content>
         </div>
       </div>
 
-      <div class="details">
+      <div class="net-work details">
         <div class="title h5">{{ title }}</div>
-        <div class="status paragraph-2">{{ on ? 'ON' : 'OFF' }}</div>
       </div>
     </nb-card>
   `,
 })
 export class StatusCardComponent {
-
   @Input() title: string;
   @Input() type: string;
   @Input() on = true;
